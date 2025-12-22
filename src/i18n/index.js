84f62b778266504379f804ee242ec5562be9a423
Dafaairpanas <dafaoffic@gmail.com>
2025-12-22 11:25:00 +0700

    fix: case-sensitive import paths for i18n files (whyUs.json, floatMessage.json)

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,28 +5,28 @@ import { initReactI18next } from "react-i18next";
 import navbar_en from "./en/navbar.json";
 import home_en from "./en/home.json";
 import about_en from "./en/about.json";
-import whyus_en from "./en/whyus.json";
+import whyus_en from "./en/whyUs.json";
 import collections_en from "./en/collections.json";
 import csr_en from "./en/csr.json";
 import news_en from "./en/news.json";
 import careers_en from "./en/careers.json";
 import contact_en from "./en/contact.json";
 import footer_en from "./en/footer.json";
-import floatmessage_en from "./en/floatmessage.json";
+import floatmessage_en from "./en/floatMessage.json";
 import articleNav_en from "./en/articleNavigation.json";
 
 // ID
 import navbar_id from "./id/navbar.json";
 import home_id from "./id/home.json";
 import about_id from "./id/about.json";
-import whyus_id from "./id/whyus.json";
+import whyus_id from "./id/whyUs.json";
 import collections_id from "./id/collections.json";
 import csr_id from "./id/csr.json";
 import news_id from "./id/news.json";
 import careers_id from "./id/careers.json";
 import contact_id from "./id/contact.json";
 import footer_id from "./id/footer.json";
-import floatmessage_id from "./id/floatmessage.json";
+import floatmessage_id from "./id/floatMessage.json";
 import articleNav_id from "./id/articleNavigation.json";
 
 i18n.use(initReactI18next).init({
